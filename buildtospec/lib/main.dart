6c965553664a6b3f6defa6ea5003ab927da389d4
Geoffrey Gracia <grg7576@rit.edu>
2024-10-01 04:35:09 -0400

    making it happen

--- buildtospec/lib/main.dart
+++ buildtospec/lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+const String loremIpsumText = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget turpis in urna aliquam fermentum. Etiam sit amet velit sit amet purus viverra dictum. Sed ultricies nunc sed augue laoreet, quis malesuada dolor vehicula.
+
+Pellentesque tincidunt, magna vel interdum convallis, velit felis vulputate leo, nec consequat ligula sapien nec libero. Nam malesuada ligula sed odio egestas, vel suscipit nulla suscipit. Praesent vehicula arcu eu interdum lobortis.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi dapibus, elit vitae malesuada tempus, orci urna efficitur mauris, sit amet elementum nunc libero et felis. Curabitur viverra risus sed tortor dictum volutpat.
+""";
+
 void main() {
   runApp(const MinecraftGuideApp());
 }
@@ -32,7 +40,7 @@ class GuideHome extends StatelessWidget {
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/valley.jpg'),
+                image: AssetImage('flutter_design_to_spec_assets/assets/images/valley.jpg'),
                 fit: BoxFit.cover,
               ),
             ),
@@ -45,7 +53,7 @@ class GuideHome extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Image.asset('assets/images/pickaxe.svg', width: 40),
+                  Image.asset('flutter_design_to_spec_assets/assets/images/pickaxe.svg', width: 40),
                   const Text(
                     'Trees & Wood',
                     style: TextStyle(
@@ -66,10 +74,10 @@ class GuideHome extends StatelessWidget {
             ),
           ),
           // Main Content
-          Positioned(
-            top: 100,
-            left: 16,
-            right: 16,
+          Expanded(
+            // top: 100,
+            // left: 16,
+            // right: 16,
             child: SingleChildScrollView(
               child: Column(
                 children: [
@@ -88,7 +96,7 @@ class GuideHome extends StatelessWidget {
                     ),
                     child: Column(
                       children: [
-                        Image.asset('assets/images/oaktree.png', width: 100),
+                        Image.asset('flutter_design_to_spec_assets/assets/images/oaktree.png', width: 500),
                         const Padding(
                           padding: EdgeInsets.all(8.0),
                           child: Text(
@@ -103,55 +111,30 @@ class GuideHome extends StatelessWidget {
                   _buildGuideSection(
                     context,
                     'Planks',
-                    'assets/images/planks.png',
-                    const Color(0xFF324E44),
-                    'Description of Planks...',
+                    'flutter_design_to_spec_assets/assets/images/planks.png',
+                    const Color(0xFF686868),
+                    loremIpsumText,
                   ),
                   _buildGuideSection(
                     context,
                     'Chest',
-                    'assets/images/chest.png',
+                    'flutter_design_to_spec_assets/assets/images/chest.png',
                     const Color(0xFF686868),
-                    'Description of Chest...',
+                    loremIpsumText,
                   ),
                   _buildGuideSection(
                     context,
                     'Stick',
-                    'assets/images/stick.png',
+                    'flutter_design_to_spec_assets/assets/images/stick.png',
                     const Color(0xFF686868),
-                    'Description of Stick...',
+                    loremIpsumText,
                   ),
                   _buildGuideSection(
                     context,
                     'Stairs',
-                    'assets/images/stairs.png',
+                    'flutter_design_to_spec_assets/assets/images/stairs.png',
                     const Color(0xFF686868),
-                    'Description of Stairs...',
-                  ),
-                  // Footer Image
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/trees.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Text(
-                          'More about Trees',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'RetroComputer',
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
+                    loremIpsumText,
                   ),
                 ],
               ),
@@ -162,13 +145,118 @@ class GuideHome extends StatelessWidget {
     );
   }
 
-  void _showAccountPopup(BuildContext context) {
+  Widget _buildGuideSection(
+    BuildContext context, 
+    String title, 
+    String imagePath, 
+    Color color, 
+    String description,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        _showDetailsPopup(context, title, imagePath, color, description);
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            // Left Side: Framed Image
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: const Color(0xFF244D3E), // Frame color
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Image.asset(imagePath, width: 60),
+            ),
+            // Right Side: Title and Scrollable Description
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  SingleChildScrollView(
+                    child: Text(
+                      description,
+                      style: const TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showDetailsPopup(BuildContext context, String title, String imagePath, Color color, String description) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Account Credentials'),
-          content: const Text('Your account credentials go here.'),
+          contentPadding: EdgeInsets.zero, // Removes default padding
+          content: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFF686868), // Gray outer box
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Item name (title) at the top
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                
+                // Green background around the image
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: color, // Green background color (same as the section)
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Image.asset(imagePath, width: 100),
+                ),
+                
+                const SizedBox(height: 16),
+                
+                // White bordered box for description text
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white, // White background
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.black, // Black text in the white box
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
           actions: [
             TextButton(
               onPressed: () {
@@ -182,43 +270,17 @@ class GuideHome extends StatelessWidget {
     );
   }
 
-  Widget _buildGuideSection(
-      BuildContext context, String title, String imagePath, Color color, String description) {
-    return GestureDetector(
-      onTap: () {
-        _showDetailsPopup(context, title, description);
-      },
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          children: [
-            Image.asset(imagePath, width: 60),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  title,
-                  style: const TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showDetailsPopup(BuildContext context, String title, String description) {
+  void _showAccountPopup(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(title),
-          content: Text(description),
+          title: const Text('About'),
+          content: const Text(
+            'Created by Geoff Gracia\n\n'
+            'Based on the work in 235\n\n'
+            'September 2024',
+          ),
           actions: [
             TextButton(
               onPressed: () {
